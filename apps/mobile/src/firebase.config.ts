761ac9e196296deedc0a8a05a425f177718076ef
Rafael Miziara <rafaelmiziara@hotmail.com>
2025-08-08 15:19:57 +0200

    setup(infra): configure environment variables
    This commit sets up environment variables for both the mobile app and backend.
    
    - Adds `.env` files for local configuration in `apps/mobile` and `packages/backend`.
    - Configures Expo to handle public keys for the mobile app.
    - Integrates `dotenv` for local development in Cloud Functions.
    - Updates `firebase.config.ts` to read keys from environment variables.
    
    Closes #3

--- apps/mobile/src/firebase.config.ts
+++ apps/mobile/src/firebase.config.ts
@@ -7,13 +7,13 @@ import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
 
 // Firebase Project Configuration
 const firebaseConfig = {
-  apiKey: 'YOUR_FIREBASE_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-  measurementId: 'YOUR_MEASUREMENT_ID',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase App
@@ -25,11 +25,9 @@ export const FIREBASE_FIRESTORE = getFirestore(FIREBASE_APP)
 export const FIREBASE_FUNCTIONS = getFunctions(FIREBASE_APP)
 
 // --- Connect to Emulators in Development ---
-
 if (__DEV__) {
   console.log('Connecting to Firebase Emulators...')
-
-  connectAuthEmulator(FIREBASE_AUTH, 'http://localhost:9099')
-  connectFirestoreEmulator(FIREBASE_FIRESTORE, 'localhost', 8080)
-  connectFunctionsEmulator(FIREBASE_FUNCTIONS, 'localhost', 5001)
+  connectAuthEmulator(FIREBASE_AUTH, process.env.EXPO_PUBLIC_NGROK_URL_AUTH)
+  connectFirestoreEmulator(FIREBASE_FIRESTORE, process.env.EXPO_PUBLIC_NGROK_URL_FIRESTORE, 80)
+  connectFunctionsEmulator(FIREBASE_FUNCTIONS, process.env.EXPO_PUBLIC_NGROK_URL_FUNCTIONS, 80)
 }
