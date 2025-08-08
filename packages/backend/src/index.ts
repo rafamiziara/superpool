d40fd3f9dad5f547bec5a6ecce3668cde6892066
Rafael Miziara <rafaelmiziara@hotmail.com>
2025-08-08 15:19:57 +0200

    setup(infra): configure environment variables
    This commit sets up environment variables for both the mobile app and backend.
    
    - Adds `.env` files for local configuration in `apps/mobile` and `packages/backend`.
    - Configures Expo to handle public keys for the mobile app.
    - Integrates `dotenv` for local development in Cloud Functions.
    - Updates `firebase.config.ts` to read keys from environment variables.
    
    Closes #3

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
+import * as dotenv from 'dotenv'
 import { setGlobalOptions } from 'firebase-functions'
+
+dotenv.config()
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
