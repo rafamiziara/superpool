2c9dd2e691342cf6cb918c8779bfbb74142b7183
Rafael Miziara <rafaelmiziara@hotmail.com>
2025-09-13 14:35:12 +0200

    refactor(mobile): add and config nativewind

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>SUPERPOOL!</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white font-bold text-2xl">SUPERPOOL</Text>
     </View>
   )
 }
